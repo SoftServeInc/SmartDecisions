f8a9c63055ed8ad07ea1df2aea6dcd7bbc205eba
ShevchukTania <taniabeley@ukr.net>
2019-09-27 12:50:24 +0300

    tab slider

--- src/js/index.js
+++ src/js/index.js
@@ -17,17 +17,42 @@ $(document).ready(function(){
     },1000);
   });
 
+  function next() {
+    if ($(':animated').length) { return null };
+
+    const parent = $('.tab-pane.show');
+    const copy = parent.find('.content .row img').first().clone();
+    parent.find('.content .row').animate({ left: '-=25%'}, 500, function(){
+      parent.find('.content .row').append(copy);
+      parent.find('.content .row img').first().remove();
+      parent.find('.content .row').css('left', '-12.5%');
+    });
+  }
+
+  function previous() {
+    if ($(':animated').length) { return null };
+
+    const parent = $('.tab-pane.show');
+    const copy = parent.find('.content .row img').last().clone();
+    parent.find('.content .row').animate({ left: '+=25%'}, 500, function(){
+      parent.find('.content .row').prepend(copy);
+      parent.find('.content .row img').last().remove();
+      parent.find('.content .row').css('left', '-12.5%');
+    });
+  }
+
   $('.tab-pane .next').on('click', function() {
-    const parent = $(this).closest('.tab-pane');
-    const copy = parent.find('.content .row img').first();
-    parent.find('.content .row').append(copy);
-    copy.html('');
+    next();
   })
 
   $('.tab-pane .previous').on('click', function() {
-    const parent = $(this).closest('.tab-pane');
-    const previous = parent.find('.content .row img').last();
-    parent.find('.content .row').prepend(previous);
-    previous.html('');
+    previous();
+  })
+
+  $("body").keydown(function(e) {
+    if (!e) e = window.event;
+    const kc = e.which;
+    if (kc == 37) previous();
+    if (kc == 39) next();
   })
 });
